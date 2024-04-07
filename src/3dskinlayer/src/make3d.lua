efa3b4d75779b3dbc1f3e5bdf960042c51e21065
Manuel-3 <58179488+Manuel-3@users.noreply.github.com>
2024-04-07 18:20:46 +0200

    Add inflate to 3d skin layers

--- src/3dskinlayer/src/make3d.lua
+++ src/3dskinlayer/src/make3d.lua
@@ -1,15 +1,37 @@
 ----   HELP   ----
--- With offset > 0 and scale = 1 the corners will be missing, can produce a more "round" effect
--- With offset = 0 and scale > 1 corners/edges that have different colors on different sides could show with only one of those colors instead
--- With offset > 0 and scale > 1 might look bad, but with only a tiny offset it can fix the corner/edge colors, but could also cause z-fighting
--- Change scale to fit what you like, also test it with armor on to check if it's clipping through
+-- Important first note: Figura currently doesn't have a way to access pixels on the vanilla skin texture,
+-- meaning low_complexity_mode can't do optimisations! It is recommended to not turn on use_vanilla_skin
+-- and instead replace the existing templatetexture.png with your skin texture!
+--
+-- It will automatically figure out if you use Steve or Alex style skin.
+--
+-- In Settings below: It's recommended to either use inflate or scale, and not mix them!
+-- Scale simply scales the outer 3d layer, while inflate scales each axis based on cube size to get a more uniform effect.
+--
+-- With inflate = 0:
+---- With offset > 0 and scale = 1 the corners will be missing, can produce a more "round" effect
+---- With offset = 0 and scale > 1 corners/edges that have different colors on different sides could show with only one of those colors instead
+---- With offset > 0 and scale > 1 might look bad, but with only a tiny offset it can fix the corner/edge colors, but could also cause z-fighting
+-- With scale = 1:
+---- Basically same as above, except with inflate this time.
+---- Note that inflate begins at 0 (no change) to >0 (inflates) instead of scale which is 1 (no change) <1 (smaller) and >1 (bigger)
+--
+-- Change scale/inflate to fit what you like, also test it with armor on to check if it's clipping through.
+-- The default values below should already look good for most cases.
 
 ---- SETTINGS ----
-local offset = 0.01 -- from 0 to 1
-local head_scale = 1.15
-local body_scale = 1.25
-local arm_scale = 1.27
-local leg_scale = 1.25
+local offset = 0.01 -- from 0 to 1, linearly moves pixels along normal axis
+
+local head_inflate = 0.8 -- 0 = no change, >0 inflates
+local body_inflate = 0.6
+local arm_inflate = 0.6
+local leg_inflate = 0.6
+
+local head_scale = 1 -- 1 = no change, <1 makes smaller, >1 makes bigger
+local body_scale = 1
+local arm_scale = 1
+local leg_scale = 1
+
 local use_vanilla_skin = false -- set to true if you want to use your vanilla skin. doesnt work with low complexity mode so it is recommended to replace the included texture with your skin instead
 local skin_resolution = 16 -- 16 for normal, 32 for a double resolution skin, etc... up to 256x (will increase complexity significantly)
 local low_complexity_mode = true -- set to false if you need to be able to animate or change textures on the fly (will increase complexity significantly)
@@ -35,6 +57,7 @@ models.player_model3d.RightLeg["Right Leg"]:setVisible(not use_vanilla_skin)
 models.player_model3d.pixel:visible(false)
 
 local px = models.player_model3d.pixel["x"..skin_resolution]:visible(true)
+local res = skin_resolution / 16
 
 function newPixel(at,north,south,east,west,up,down)
     local pixel = at:newPart("pixel")
@@ -67,9 +90,10 @@ end
 ---@param west Vector4
 ---@param up Vector4
 ---@param down Vector4
-local function Make3d(at, dims, north, east, south, west, up, down)
+local function Make3d(at, scale, inflate, dims, north, east, south, west, up, down)
     local p = at:getTruePivot()
     local texture = use_vanilla_skin and {getPixel=function(_,x,y)return {a=1} end} or at:getTextures()[1]
+    at:setScale(scale*(1+inflate/dims.x)/res, scale*(1+inflate/dims.y)/res, scale*(1+inflate/dims.z)/res)
     local function needsN(x, y, u, v, w, h)
         if use_vanilla_skin then return {true, false, true, true, true, true} end
         return {
@@ -199,33 +223,23 @@ local function Make3d(at, dims, north, east, south, west, up, down)
 end
 
 function events.ENTITY_INIT()
-    local res = skin_resolution / 16
     if player:getModelType() == "SLIM" then
         models.player_model3d.LeftArm:setVisible(false)
         models.player_model3d.RightArm:setVisible(false)
-        Make3d(models.player_model3d.LeftArmSlim.center8, vec(3,12,4)*res, vec(52,52,3,12)*res, vec(48,52,4,12)*res, vec(59,52,3,12)*res, vec(55,52,4,12)*res, vec(55,52,-3,-4)*res, vec(58,48,-3,4)*res)
-        Make3d(models.player_model3d.RightArmSlim.center7, vec(3,12,4)*res, vec(44,36,3,12)*res, vec(40,36,4,12)*res, vec(51,36,3,12)*res, vec(47,36,4,12)*res, vec(47,36,-3,-4)*res, vec(50,32,-3,4)*res)
+        Make3d(models.player_model3d.LeftArmSlim.center8, arm_scale, arm_inflate, vec(3,12,4)*res, vec(52,52,3,12)*res, vec(48,52,4,12)*res, vec(59,52,3,12)*res, vec(55,52,4,12)*res, vec(55,52,-3,-4)*res, vec(58,48,-3,4)*res)
+        Make3d(models.player_model3d.RightArmSlim.center7, arm_scale, arm_inflate, vec(3,12,4)*res, vec(44,36,3,12)*res, vec(40,36,4,12)*res, vec(51,36,3,12)*res, vec(47,36,4,12)*res, vec(47,36,-3,-4)*res, vec(50,32,-3,4)*res)
     else
         models.player_model3d.LeftArmSlim:setVisible(false)
         models.player_model3d.RightArmSlim:setVisible(false)
-        Make3d(models.player_model3d.LeftArm.center5, vec(4,12,4)*res, vec(52,52,4,12)*res, vec(48,52,4,12)*res, vec(60,52,4,12)*res, vec(56,52,4,12)*res, vec(56,52,-4,-4)*res, vec(60,48,-4,4)*res)
-        Make3d(models.player_model3d.RightArm.center4, vec(4,12,4)*res, vec(44,36,4,12)*res, vec(40,36,4,12)*res, vec(52,36,4,12)*res, vec(48,36,4,12)*res, vec(48,36,-4,-4)*res, vec(52,32,-4,4)*res)
+        Make3d(models.player_model3d.LeftArm.center5, arm_scale, arm_inflate, vec(4,12,4)*res, vec(52,52,4,12)*res, vec(48,52,4,12)*res, vec(60,52,4,12)*res, vec(56,52,4,12)*res, vec(56,52,-4,-4)*res, vec(60,48,-4,4)*res)
+        Make3d(models.player_model3d.RightArm.center4, arm_scale, arm_inflate, vec(4,12,4)*res, vec(44,36,4,12)*res, vec(40,36,4,12)*res, vec(52,36,4,12)*res, vec(48,36,4,12)*res, vec(48,36,-4,-4)*res, vec(52,32,-4,4)*res)
     end
-    Make3d(models.player_model3d.Head.center2 , vec(8,8,8)*res, vec(40, 8, 8, 8)*res, vec(32, 8, 8, 8)*res, vec(56, 8, 8, 8)*res, vec(48, 8, 8, 8)*res, vec(48, 8, -8, -8)*res, vec(56, 0, -8, 8)*res)
-    Make3d(models.player_model3d.Body.center3, vec(8,12,4)*res, vec(20,36,8,12)*res, vec(16,36,4,12)*res, vec(32,36,8,12)*res, vec(28,36,4,12)*res, vec(28,36,-8,-4)*res, vec(36,32,-8,4)*res)
-    Make3d(models.player_model3d.LeftLeg.center, vec(4,12,4)*res, vec(4,52,4,12)*res, vec(0,52,4,12)*res, vec(12,52,4,12)*res, vec(8,52,4,12)*res, vec(8,52,-4,-4)*res, vec(12,48,-4,4)*res)
-    Make3d(models.player_model3d.RightLeg.center6, vec(4,12,4)*res, vec(4,36,4,12)*res, vec(0,36,4,12)*res, vec(12,36,4,12)*res, vec(8,36,4,12)*res, vec(8,36,-4,-4)*res, vec(12,32,-4,4)*res)    
+    Make3d(models.player_model3d.Head.center2, head_scale, head_inflate, vec(8,8,8)*res, vec(40, 8, 8, 8)*res, vec(32, 8, 8, 8)*res, vec(56, 8, 8, 8)*res, vec(48, 8, 8, 8)*res, vec(48, 8, -8, -8)*res, vec(56, 0, -8, 8)*res)
+    Make3d(models.player_model3d.Body.center3, body_scale, body_inflate, vec(8,12,4)*res, vec(20,36,8,12)*res, vec(16,36,4,12)*res, vec(32,36,8,12)*res, vec(28,36,4,12)*res, vec(28,36,-8,-4)*res, vec(36,32,-8,4)*res)
+    Make3d(models.player_model3d.LeftLeg.center, leg_scale, leg_inflate, vec(4,12,4)*res, vec(4,52,4,12)*res, vec(0,52,4,12)*res, vec(12,52,4,12)*res, vec(8,52,4,12)*res, vec(8,52,-4,-4)*res, vec(12,48,-4,4)*res)
+    Make3d(models.player_model3d.RightLeg.center6, leg_scale, leg_inflate, vec(4,12,4)*res, vec(4,36,4,12)*res, vec(0,36,4,12)*res, vec(12,36,4,12)*res, vec(8,36,4,12)*res, vec(8,36,-4,-4)*res, vec(12,32,-4,4)*res)    
 
     if use_vanilla_skin then
         models.player_model3d:setPrimaryTexture("SKIN")
     end
-    
-    models.player_model3d.Head.center2:setScale(head_scale/res)
-    models.player_model3d.Body.center3:setScale(body_scale/res)
-    models.player_model3d.LeftLeg.center:setScale(leg_scale/res)
-    models.player_model3d.RightLeg.center6:setScale(leg_scale/res)
-    models.player_model3d.LeftArm.center5:setScale(arm_scale/res)
-    models.player_model3d.LeftArmSlim.center8:setScale(arm_scale/res)
-    models.player_model3d.RightArm.center4:setScale(arm_scale/res)
-    models.player_model3d.RightArmSlim.center7:setScale(arm_scale/res)
 end
