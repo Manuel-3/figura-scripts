3ee15ab43cc5a927b69b2c3742d12f97d9f771e2
Manuel_ <58179488+Manuel-3@users.noreply.github.com>
2024-09-29 23:14:49 +0200

    confetti update
    emissive flag
    billboard mesh
    ticker and renderer
    better examples

--- src/confetti/example/confetti.lua
+++ src/confetti/example/confetti.lua
@@ -5,25 +5,95 @@ local Confetti = {}
 
 local Particles = {}
 local Instances = {}
+local modelinstances = models:newPart("confetti"..client.intUUIDToString(client.generateUUID())):setParentType("World"):newPart("Instances")
+local DEFAULT_LIFETIME = 20
 
-if models.confetti then
-    error("There is already a confetti.bbmodel. Please remove or rename it.")
+---Default ticker
+---@param instance Confetto
+function Confetti.defaultTicker(instance)
+    instance._position = instance.position
+    instance._rotation = instance.rotation
+    instance._scale = instance.scale
+    instance.velocity = instance.velocity + instance.options.acceleration
+    instance.velocity = instance.velocity * instance.options.friction
+    instance.position = instance.position + instance.velocity
+    instance.scale = instance.scale + instance.options.scaleOverTime
+    instance.rotation = instance.rotation + instance.options.rotationOverTime
 end
-models:newPart("confetti")
-models.confetti:newPart("Instances")
-models.confetti:newPart("SpriteTaskTemplate")
-models.confetti:setParentType("World")
+
+---Default renderer
+---@param instance Confetto
+function Confetti.defaultRenderer(instance, delta, context, matrix)
+    instance.mesh:setPos((math.lerp(instance._position,instance.position,delta))*16)
+    instance.mesh:setRot(math.lerp(instance._rotation,instance.rotation,delta))
+    instance.mesh:setScale(math.lerp(instance._scale,instance.scale,delta))
+end
 
 ---@class ConfettoOptions
----@field lifetime number|nil Lifetime in ticks
+---@field lifetime number|nil Initial lifetime in ticks
 ---@field acceleration Vector3|number|nil Vector in world space or a number which accelerates forwards (positive) or backwards (negative) in the current movement direction
 ---@field friction number|nil Number of friction to slow down the particle. Value of 1 is no friction, value <1 slows it down, value >1 speeds it up.
 ---@field scale Vector3|number|nil Initial scale when spawning
 ---@field scaleOverTime Vector3|number|nil Change of scale every tick
 ---@field rotation Vector3|number|nil Initial rotation when spawning
 ---@field rotationOverTime Vector3|number|nil Change of rotation every tick
----@field billboard boolean|nil Whether the Sprite should always face the camera, only affects Sprite particles
+---@field billboard boolean|nil Makes the particle always face the camera
+---@field emissive boolean|nil Makes the particle emissive, only works for mesh. Alternative for sprite is to make a flat cube with the sprite on it as a mesh particle.
+---@field ticker function|nil Function called each tick. To keep default behavior, call `Confetti.defaultTicker(particle)` before your own code.
+---@field renderer function|nil Function called each frame. To keep default behavior, call `Confetti.defaultRenderer(particle, delta, context, matrix)` before your own code.
 local ConfettoOptions = {}
+ConfettoOptions.__index = ConfettoOptions
+
+local DefaultConfettoOptions = {
+    lifetime = DEFAULT_LIFETIME,
+    acceleration = vec(0,0,0),
+    friction = 1,
+    scale = vec(1,1,1),
+    scaleOverTime = vec(0,0,0),
+    rotation = vec(0,0,0),
+    rotationOverTime = vec(0,0,0),
+    billboard=false,
+    emissive=false,
+    ticker=Confetti.defaultTicker,
+    renderer=Confetti.defaultRenderer
+}
+
+---@class Confetto
+---@field mesh ModelPart The model part
+---@field task SpriteTask|nil The sprite task if it's a sprite particle
+---@field position Vector3 Current position in world coordinates
+---@field _position Vector3 Last tick position
+---@field lifetime number Remaining lifetime in ticks
+---@field scale Vector3|number Current scale
+---@field _scale Vector3|number Last tick scale
+---@field rotation Vector3|number Current rotation
+---@field _rotation Vector3|number Last tick rotation
+---@field options ConfettoOptions
+local Confetto = {}
+Confetto.__index = Confetto
+
+function Confetto:new(mesh, task, pos, vel, bounds, pivot, options)
+    return setmetatable({
+        mesh=mesh,
+        task=task,
+        position=pos,
+        _position=pos,
+        velocity=vel,
+        lifetime=options.lifetime,
+        scale=options.scale,
+        _scale=options.scale,
+        rotation=options.rotation,
+        _rotation=options.rotation,
+        bounds=bounds,
+        pivot=pivot,
+        options=options
+    }, Confetto)
+end
+
+function Confetto:setUVOffset(u,v)
+    self.u = u
+    self.v = v
+end
 
 --- Register a Mesh Particle
 ---@param name string
@@ -33,7 +103,7 @@ local ConfettoOptions = {}
 function Confetti.registerMesh(name, mesh, lifetime)
     assert(mesh, "Model Part does not exist! Double check the path, spelling, and if you saved your model file.")
     if mesh:getType() ~= "GROUP" then logJson('[{color="yellow",text="[WARNING] "},{color:"white",text:"You are creating a particle by targeting a model part directly, instead of a group. This can cause unexpected behavior. It is recommended to use a group that is positioned at (0,0,0) instead. If you know what you are doing, to get rid of this warning simply delete this line of code."}]') end
-    Particles[name] = {mesh=mesh,lifetime=lifetime or 20}
+    Particles[name] = {mesh=mesh,lifetime=lifetime or DEFAULT_LIFETIME}
     mesh:setVisible(false)
 end
 
@@ -49,7 +119,7 @@ function Confetti.registerSprite(name, sprite, bounds, lifetime, pivot)
         logTable(textures:getTextures())
         error("Texture does not exist. Use the correct name shown in the list above. It may need a model name before the texture name separated by a dot.")
     end
-    Particles[name] = {sprite=sprite,bounds=bounds,lifetime=lifetime or 20,pivot=pivot or vec((bounds.y-bounds.x)/2,(bounds.w-bounds.z)/2)}
+    Particles[name] = {sprite=sprite,bounds=bounds,lifetime=lifetime or DEFAULT_LIFETIME,pivot=pivot or vec((bounds.y-bounds.x)/2,(bounds.w-bounds.z)/2)}
 end
 
 --- Spawn a registered custom particle
@@ -59,105 +129,51 @@ end
 ---@param options ConfettoOptions|nil
 ---@return Confetto
 function Confetti.newParticle(name, pos, vel, options)
-    -- Handle Overloads
-    if vel == nil then
-        vel = vec(0,0,0)
+    vel = vel or vec(0,0,0)
+    options = options or {}
+    options.lifetime = options.lifetime or Particles[name].lifetime
+    setmetatable(options, { __index = DefaultConfettoOptions })
+    if type(options.scaleOverTime) == "number" then
+        options.scaleOverTime = vec(options.scaleOverTime,options.scaleOverTime,options.scaleOverTime)
     end
-    if options == nil then
-        options = {}
-    end
-    local scale = options.scale or vec(1,1,1)
-    local rotation = options.rotation or vec(0,0,0)
-    -- Spawn Particle
-    local meshInstance = nil
-    if Particles[name].mesh ~= nil then
-        meshInstance = Particles[name].mesh:copy("a")
-        meshInstance:setVisible(true)
-        models.confetti.Instances:addChild(meshInstance)
-    else
-        meshInstance = models.confetti["SpriteTaskTemplate"]:copy("a")
-        local taskholder = models.confetti["SpriteTaskTemplate"]:copy("a")
-        if options.billboard then taskholder:setParentType("Camera") end
-        meshInstance:addChild(taskholder)
-        models.confetti.Instances:addChild(meshInstance)
-        local task = taskholder:newSprite("a")
-        task:setPos(Particles[name].pivot.xy_)
-        task:setTexture(Particles[name].sprite)
-        task:setDimensions(Particles[name].sprite:getDimensions())
-        task:setUVPixels(Particles[name].bounds.x,Particles[name].bounds.y)
-        task:setRegion(Particles[name].bounds.z+1-Particles[name].bounds.x,Particles[name].bounds.w+1-Particles[name].bounds.y)
-        task:setSize(Particles[name].bounds.z+1-Particles[name].bounds.x,Particles[name].bounds.w+1-Particles[name].bounds.y)
+    if type(options.rotationOverTime) == "number" then
+        options.rotationOverTime = vec(options.rotationOverTime,options.rotationOverTime,options.rotationOverTime)
     end
     if type(options.acceleration) == "number" then
         options.acceleration = vel:normalized() * options.acceleration
     end
-    ---@class Confetto
-    ---@field mesh ModelPart
-    ---@field sprite Texture|nil
-    ---@field bounds Vector4
-    ---@field lifetime number
-    ---@field options ConfettoOptions
-    ---@field position Vector3
-    ---@field rotation Vector3|number
-    ---@field scale Vector3|number
-    ---@field private _position any
-    ---@field private _rotation any
-    ---@field private _scale any
-    local particle = {
-        mesh=meshInstance,
-        sprite=Particles[name].sprite,
-        bounds=Particles[name].bounds,
-        lifetime=options.lifetime or Particles[name].lifetime,
-        options=options,
-        position = pos,
-        _position = pos,
-        velocity = vel,
-        scale=scale,
-        _scale=scale,
-        rotation=rotation,
-        _rotation=rotation
-    }
-    table.insert(Instances,particle)
+    local meshInstance, task
+    if Particles[name].mesh ~= nil then
+        meshInstance = modelinstances:newPart("_")
+        Particles[name].mesh:copy("meshholder"):moveTo(meshInstance):setParentType(options.billboard and "CAMERA" or "NONE"):setVisible(true)
+    else
+        meshInstance = modelinstances:newPart("_")
+        task = meshInstance:newPart("taskholder")
+            :setParentType(options.billboard and "CAMERA" or "NONE")
+            :newSprite("_")
+                :setPos(Particles[name].pivot.xy_)
+                :setTexture(Particles[name].sprite)
+                :setDimensions(Particles[name].sprite:getDimensions())
+                :setUVPixels(Particles[name].bounds.x,Particles[name].bounds.y)
+                :setRegion(Particles[name].bounds.z+1-Particles[name].bounds.x,Particles[name].bounds.w+1-Particles[name].bounds.y)
+                :setSize(Particles[name].bounds.z+1-Particles[name].bounds.x,Particles[name].bounds.w+1-Particles[name].bounds.y)
+    end
+    if options.emissive then
+        meshInstance:setSecondaryTexture("PRIMARY")
+    end
+    local particle = Confetto:new(meshInstance, task, pos, vel, Particles[name].bounds, Particles[name].pivot, options)
+    table.insert(Instances, particle)
     return particle
 end
 
 function events.TICK()
     local deleted = {}
-    for i, value in ipairs(Instances) do
-        value._position = value.position
-        value._rotation = value.rotation
-        value._scale = value.scale
-
-        if value.options.acceleration ~= nil then
-            value.velocity = value.velocity + value.options.acceleration
-        end
-
-        if value.options.friction ~= nil then
-            value.velocity = value.velocity * value.options.friction
-        end
-
-        value.position = value.position + value.velocity
-
-        if value.options.scaleOverTime ~= nil then
-            if type(value.options.scaleOverTime) == "number" then
-                value.scale = value.scale + vec(value.options.scaleOverTime,value.options.scaleOverTime,value.options.scaleOverTime)
-            else
-                value.scale = value.scale + value.options.scaleOverTime
-            end
-        end
-
-        if value.options.rotationOverTime ~= nil then
-            if type(value.options.rotationOverTime) == "number" then
-                value.rotation = value.rotation + vec(value.options.rotationOverTime,value.options.rotationOverTime,value.options.rotationOverTime)
-            else
-                value.rotation = value.rotation + value.options.rotationOverTime
-            end
-        end
-
-        value.lifetime = value.lifetime - 1
-        if value.lifetime <= 0 then
+    for i, instance in ipairs(Instances) do
+        instance.options.ticker(instance)
+        instance.lifetime = instance.lifetime - 1
+        if instance.lifetime <= 0 then
             table.insert(deleted,i)
-            models.confetti.Instances:removeChild(value.mesh)
+            modelinstances:removeChild(instance.mesh)
         end
     end
     for i, key in ipairs(deleted) do
@@ -165,13 +181,9 @@ function events.TICK()
     end
 end
 
-function events.RENDER(delta)
-    for i, value in ipairs(Instances) do
-        if value.mesh ~= nil then
-            value.mesh:setPos((math.lerp(value._position,value.position,delta))*16)
-            value.mesh:setRot(math.lerp(value._rotation,value.rotation,delta))
-            value.mesh:setScale(math.lerp(value._scale,value.scale,delta))
-        end
+function events.RENDER(delta, context, matrix)
+    for _, instance in ipairs(Instances) do
+        instance.options.renderer(instance, delta, context, matrix)
     end
 end
 
--- src/confetti/example/script.lua
+++ src/confetti/example/script.lua
@@ -1,21 +1,53 @@
+-- EXAMPLE SCRIPT --
+-- If you want to view an example for longer time, change the example number below, and the time to some big value
+-- By the way, all newParticle lines are called in a tick event!
+local example = 1
+local time_each_example = 100
+
+-- Example code begins here --
 local confetti = require("confetti")
 
 confetti.registerMesh("spark", models.model.Spark)
-confetti.registerSprite("leaf", textures["mytexture"], vec(0,5,4,9), 30)
+confetti.registerSprite("leaf", textures["model.mytexture"], vec(0,5,4,9), 30)
+confetti.registerSprite("fire", textures["model.mytexture"], vec(0,10,4,15))
 
-function events.TICK()    
-    
-    -- Example 1
+function example1()
+    -- Example 1 bouncing sparks
     confetti.newParticle(
         "spark",
         player:getPos()+vec(0,math.random(),0),
-        vec((math.random()-0.5)*0.3,math.random()*0.3,(math.random()-0.5)*0.3),
+        vec((math.random()-0.5)*0.3,0.2+math.random()*0.2,(math.random()-0.5)*0.3),
         {
-            acceleration=vec(0,-0.04,0)
+            acceleration=vec(0,-0.04,0),
+            lifetime=50,
+            emissive=true,
+            ticker=function(particle)
+                -- make sure to call the default ticker which will calculate the regular particle movement
+                confetti.defaultTicker(particle)
+                -- get velocity so we can check for collision
+                local x,y,z = particle.velocity:unpack()
+                -- check if previous tick _position plus velocity in each direction would be inside a block, if so flip the velocity to bounce
+                if world.getBlockState(particle._position+vec(x,0,0)):isSolidBlock() or world.getBlockState(particle._position-vec(x,0,0)):isSolidBlock() then
+                    particle.velocity.x = -x
+                end
+                if world.getBlockState(particle._position+vec(0,y,0)):isSolidBlock() or world.getBlockState(particle._position-vec(0,y,0)):isSolidBlock() then
+                    particle.velocity.y = -y*0.7 -- for y velocity slow down when bouncing
+                end
+                if world.getBlockState(particle._position+vec(0,0,z)):isSolidBlock() or world.getBlockState(particle._position-vec(0,0,z)):isSolidBlock() then
+                    particle.velocity.z = -z
+                end
+                -- overwrite position with our own calculation.
+                -- using _position (which is the position from previous tick) because
+                -- the defaultTicker() already modified the current position,
+                -- so we want to overwrite that with the previous tick value as a base
+                particle.position = particle._position + particle.velocity
+            end
         }
     )
+end
 
-    -- Example 2
+function example2()
+    -- Example 2 leaves falling
     confetti.newParticle(
         "leaf",
         player:getPos()+vec(math.random()*2-1,4,math.random()*2-1),
@@ -24,73 +56,142 @@ function events.TICK()
             rotationOverTime=10+math.random()*5
         }
     )
-
-    -- Example 3
-    -- confetti.newParticle(
-    --     "spark",
-    --     player:getPos()+vec(0,math.random(),0),
-    --     vec((math.random()-0.5)*0.3,math.random()*0.3,(math.random()-0.5)*0.3),
-    --     {
-    --         friction=0.83
-    --     }
-    -- )
-
-    -- Example 4
-    -- confetti.newParticle(
-    --     "spark",
-    --     player:getPos()+vec(0,math.random(),0),
-    --     vec((math.random()-0.5)*0.0001,math.random()*0.0001,(math.random()-0.5)*0.0001),
-    --     {
-    --         rotationOverTime=30,
-    --         acceleration=0.03
-    --     }
-    -- )
-
-    -- Example 5
-    -- confetti.newParticle("spark",player:getPos()+vec(0,math.random(),0),vec(0,0,0))
-
-    -- Example 6
-    -- confetti.newParticle(
-    --     "leaf",
-    --     player:getPos()+vec(0,math.random(),0),
-    --     vec((math.random()-0.5)*0.2,math.random()*0.2,(math.random()-0.5)*0.2),
-    --     {
-    --         billboard = true,
-    --         scale=0.5+math.random()*0.5
-    --     }
-    -- )
-
-    -- Example 7
-    -- confetti.newParticle(
-    --     "spark",
-    --     player:getPos()+vec(0,math.random(),0),
-    --     vec((math.random()-0.5)*0.2,math.random()*0.2,(math.random()-0.5)*0.2),
-    --     {
-    --         scale=3,
-    --         scaleOverTime=-0.2,
-    --     }
-    -- )
-
 end
 
--- Example 8
--- local myparticles = {}
--- function events.ENTITY_INIT()
---     for _=0,20 do
---         table.insert(myparticles,confetti.newParticle(
---             "spark",
---             player:getPos():add(0,1,0),
---             vec((math.random()-0.5)*0.3,math.random()*0.3,(math.random()-0.5)*0.3)
---         ))
---     end
--- end
--- function events.TICK()
---     for _, particle in ipairs(myparticles) do
---         -- keep lifetime above 1
---         particle.lifetime = 2
---         -- accelerate towards player
---         particle.options.acceleration = (player:getPos():add(0,1,0)-particle.position):normalized()*0.05
---         -- set a maximum speed
---         particle.velocity = particle.velocity:normalized()*math.min(particle.velocity:length(), 0.8)
---     end
--- end
+function example3()
+    -- Example 3 animated texture
+    confetti.newParticle(
+        "fire",
+        player:getPos()+vec(1-math.random()*2,0.1+math.random()*2,1-math.random()*2),
+        vec(0,0,0),
+        {
+            scale=0.6,
+            frame=0, -- custom variable "frame" added into options
+            billboard=true,
+            ticker=function(particle)
+                -- we dont need the default ticker in this case, but in general always call it
+                -- check out the code to see what it does to determine if you need it
+
+                -- set uv to original bounds plus a frame offset
+                particle.task:setUVPixels(particle.bounds.x+particle.options.frame*5, particle.bounds.y)
+                -- increase animation frame
+                if world.getTime()%2==0 then -- slows down the animation because i like it better
+                    particle.options.frame = particle.options.frame + 1
+                end
+                -- cycle back to first frame, i need this because my texture is a weird size
+                if particle.options.frame == 3 then
+                    particle.options.frame = 0
+                end
+            end
+        }
+    )
+end
+
+function example4()
+    -- Example 4 sparks reversing and rotating wildly
+    confetti.newParticle(
+        "spark",
+        player:getPos()+vec(0,math.random(),0),
+        vec(0.25-math.random()*0.5,0.25-math.random()*0.5,0.25-math.random()*0.5),
+        {
+            rotationOverTime=30,
+            acceleration=-0.02,
+            lifetime=30,
+            emissive=true
+        }
+    )
+end
+
+function example5()
+    -- Example 5 trail when walking around
+    confetti.newParticle("spark",player:getPos()+vec(0,math.random(),0),vec(0,0,0),{emissive=true})
+end
+
+function example6()
+    -- Example 6 billboard
+    confetti.newParticle(
+        "leaf",
+        player:getPos()+vec(0,math.random(),0),
+        vec((math.random()-0.5)*0.2,math.random()*0.2,(math.random()-0.5)*0.2),
+        {
+            billboard = true,
+            scale=0.5+math.random()*0.5
+        }
+    )
+end
+
+function example7()
+    -- Example 7 sparks getting bigger
+    confetti.newParticle(
+        "spark",
+        player:getPos()+vec(0,math.random(),0),
+        vec((math.random()-0.5)*0.2,math.random()*0.2,(math.random()-0.5)*0.2),
+        {
+            scale=0,
+            scaleOverTime=0.1,
+            emissive=true
+        }
+    )
+end
+
+function example8()
+    -- Example 8 sparks slowing down and getting smaller at the end
+    confetti.newParticle(
+        "spark",
+        player:getPos()+vec(0,math.random(),0),
+        vec((math.random()-0.5)*0.3,math.random()*0.3,(math.random()-0.5)*0.3),
+        {
+            emissive=true,
+            friction=0.83,
+            ticker=function(particle)
+                confetti.defaultTicker(particle)
+                -- some fancy math stuff to only scale it near the end of lifetime
+                particle.scale = math.clamp(math.map(particle.lifetime, particle.options.lifetime, 2, 4, 0), 0, 1)
+            end
+        }
+    )
+end
+
+-- Example 9 cloud of sparks following player
+-- Note that this one is not in tick event because 
+-- these particles will stick around permanently
+function events.ENTITY_INIT()
+    for _=0,30 do
+        confetti.newParticle(
+            "spark",
+            player:getPos():add(0,1,0),
+            vec((math.random()-0.5)*0.3,math.random()*0.3,(math.random()-0.5)*0.3),
+            {
+                emissive=true,
+                friction=0.99, -- makes them slow down when you stand still for a while
+                ticker=function(particle)
+                    confetti.defaultTicker(particle)
+                    -- this one is always active, so for the example switcher its visibility is being set here
+                    particle.mesh:setVisible(example_9_currently_showing)
+                    -- set lifetime to at least two all the time so they never disappear
+                    particle.lifetime = 2
+                    -- accelerate towards the player
+                    particle.options.acceleration = (player:getPos():add(0,1,0)-particle.position):normalized()*0.05
+                    particle.velocity = particle.velocity:normalized()*math.min(particle.velocity:length(), 0.8)
+                end
+            }
+        )
+    end
+end
+
+-- Code below just switches between the examples
+local num_examples = 9
+local t = 0
+example = example - 1
+example_9_currently_showing = false
+function events.TICK()
+    if t%time_each_example==0 then
+        example = (example%num_examples)+1
+        log("Showing example "..example)
+    end
+    t = t + 1
+    example_9_currently_showing = example == 9
+    if not example_9_currently_showing then
+        _G["example"..example]()
+    end
+end
