74dd8cf9ddfaff9077cc4832bf130fbf719196d3
Manuel_ <58179488+Manuel-3@users.noreply.github.com>
2025-05-04 13:11:41 +0200

    Update swingphysics example description

--- src/swingingphysics/example/script.lua
+++ src/swingingphysics/example/script.lua
@@ -4,13 +4,16 @@ local swingOnBody = SwingingPhysics.swingOnBody
 
 -- Example usage --
 
--- Syntax: swingOnHead(modelpart, direction, limits, root, depth)
+-- Syntax: swingOnHead(modelpart, direction, limits, root, depth, globalLimits)
 -- modelpart: the modelpart to swing
 -- direction: basically imagine it dangling from a stick thats pointing in this direction
--- 0 means forward, 45 is 45 degree to the left, 90 is 90 degree to the left, and so on all the way around
+--     0 means forward, 45 is 45 degree to the left, 90 is 90 degree to the left, and so on all the way around
 -- limits: limit rotation for each axis, table layout {xLow, xHigh, yLow, yHigh, zLow, zHigh} (optional)
 -- root: if chaining, put the root group of the chain
--- depth: for each chain element increase this number. used for increasing friction to make it look better. recommended to play around with it a bit to find values you like, also dont make it too high otherwise it will almost look stiff. mostly good values are between 1 and 5
+-- depth: for each chain element increase this number. used for increasing friction to make it look better. recommended to play around with it a bit to find values you like,
+--     also dont make it too high otherwise it will almost look stiff. mostly good values are between 1 and 5
+-- globalLimits: either true or false, if set to true it will negate the vanilla model rotation before applying the limits. this means if you for example look up or
+--     down, the limits arent local to the head rotation. this can help preventing it going into other body parts when the limits were only designed for the default head position.
 
 -- Demonstration of collision with head (walk side to side to test):
 swingOnHead(models.example.Head.left, 90, {-90,90,-90,90,-90,10})
