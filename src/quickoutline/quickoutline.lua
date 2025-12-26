5781f2e434717151d0c98000b5f0de60ec4104d5
Manuel_ <58179488+Manuel-3@users.noreply.github.com>
2025-12-26 14:00:37 +0100

    Quickoutline improvements
    Normalize scaling for uneven sized cubes. Center pivot correctly for rotated cubes. Set all normals to up to remove individual face shading. Invert individual faces instead of negative scale to support asymetric meshes.

--- src/quickoutline/quickoutline.lua
+++ src/quickoutline/quickoutline.lua
@@ -5,17 +5,12 @@ local texture = textures:newTexture("white_pixel",1,1)
 texture:setPixel(0,0,vec(1,1,1))
 texture:update()
 
-local function centerPivot(cube)
-    local center = vec(0,0,0)
-    local n = 0
-    for _, vertices in pairs(cube:getAllVertices()) do
-        for _, vertex in ipairs(vertices) do
-            n = n + 1
-            center = center + vertex:getPos()
-        end
-    end
-    center = center / vec(n,n,n)
-    return center
+local function minVec(x,y)
+    return vec(math.min(x.x,y.x),math.min(x.y,y.y),math.min(x.z,y.z))
+end
+
+local function maxVec(x,y)
+    return vec(math.max(x.x,y.x),math.max(x.y,y.y),math.max(x.z,y.z))
 end
 
 --- Creates an outline model part with a specified thickness and color
@@ -41,8 +36,62 @@ local function createOutline(cube, thickness, color, emissive)
         if emissive then outline:setSecondaryTexture("CUSTOM", texture) end
         outline:setColor(color)
         outline:setPrimaryRenderType("CUTOUT_CULL")
-        outline:setScale(-1-thickness)
-        outline:setPivot(centerPivot(outline))
+        local min = vec(math.huge,math.huge,math.huge)
+        local max = vec(-math.huge,-math.huge,-math.huge)
+        local newCenter = vec(0,0,0)
+        local rotMat = matrices.rotation3(outline:getRot())
+        local pivot = outline:getPivot()
+        local invRotMat = rotMat:transposed()
+        local n = 0
+        for _, vertices in pairs(outline:getAllVertices()) do
+            for _, vertex in ipairs(vertices) do
+                n = n + 1
+                local vPos = vertex:getPos()
+                local transformedPos = (((vPos - pivot) * rotMat) + pivot)
+                newCenter = newCenter + transformedPos
+                vertex:setNormal(0,1,0)
+                min = minVec(min, vPos)
+                max = maxVec(max, vPos)
+            end
+        end
+        newCenter = newCenter / n
+        if outline:getType() ~= "MESH" then
+            outline:setPivot(newCenter)
+            for _, vertices in pairs(outline:getAllVertices()) do
+                for _, vertex in ipairs(vertices) do
+                    -- credit to soomuchlag for the pivot transformation
+                    local transformedPos = (((vertex:getPos() - pivot) * rotMat) + pivot)
+                    vertex:setPos((transformedPos - newCenter) * invRotMat + newCenter)
+                end
+            end
+        end
+        local size = max-min
+        local ref = math.max(size.x,size.y,size.z)
+        local scaling = vec(ref,ref,ref) / size
+        outline:setScale(-1-thickness*scaling)
+        if outline:getType() == "MESH" then
+            outline:setScale(outline:getScale()*-1)
+            for _, vertices in pairs(outline:getAllVertices()) do
+                local m = 1
+                while true do
+                    if not pcall(function()
+                        local v1 = vertices[m]
+                        local v2 = vertices[m+1]
+                        local v3 = vertices[m+2]
+                        local v4 = vertices[m+3]
+                        local p1 = v1:getPos()
+                        local p2 = v2:getPos()
+                        local p3 = v3:getPos()
+                        local p4 = v4:getPos()                
+                        v1:setPos(p4)
+                        v2:setPos(p3)
+                        v3:setPos(p2)
+                        v4:setPos(p1)
+                        m = m + 4
+                    end) then break end
+                end
+            end
+        end
         table.insert(outlines, outline)
     end
     return outlines
--- src/quickoutline/readme.md
+++ src/quickoutline/readme.md
@@ -2,15 +2,17 @@
 
 Replaces all the manual steps, that you would need to take in BlockBench to create outlines, with a single line of code instead.
 
-This script does *exactly* what you could manually do in BlockBench, nothing more! If you want a different version of the outline that has a script that attempts to fix a few visual glitches that appear with this method check out: https://discord.com/channels/1129805506354085959/1146318090771103764 <-- alternative version
+This script does *exactly* what you could manually do in BlockBench, nothing more! If you want a different version of the outline that has a script that attempts to fix a few visual glitches that appear with many overlapping cubes with this method check out: https://discord.com/channels/1129805506354085959/1146318090771103764 <-- alternative version
 
 Example Usage:
 ```lua
 local outline = require("quickoutline")
 
 local head_outlines_array = outline.createOutline(models.model.Head.cube, 0.2, vec(0,0,0), false)
---                                         ^cube/group    thickness^    ^color      ^emissive
+--                                                ^cube/group    thickness^    ^color      ^emissive
 ```
 If you select a group it will make outlines for all cubes within it.
+
+If you use meshes, make sure you center the pivot point of the mesh in blockbench first.
 
 ![Quick Outline Logo](./images/image.png)
