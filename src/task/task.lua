0b8662a23ebe723b067bcd75808f6dfe4aee243f
Manuel_ <58179488+Manuel-3@users.noreply.github.com>
2026-06-23 01:57:43 +0200

    Add SingleRender and event warnings to task.lua

--- src/task/task.lua
+++ src/task/task.lua
@@ -2,9 +2,45 @@
 
 local INSTRUCTION_CUTOFF_PERCENT = 0.7
 local DISABLE_PROGRESS_BARS = false
+local ENABLE_EVENT_WARNINGS = true
 
 local stack = {}
+local render_events = {}
+local events_registered = false
 
+-- Event log for warnings, the following lines can be deleted if you wish to do so
+local filename = ({...})[2]
+local function check_event(event)
+    if events_registered and ENABLE_EVENT_WARNINGS then
+        event = string.upper(event)
+        if event == "TICK" or event == "RENDER" then
+            ENABLE_EVENT_WARNINGS = false
+            logJson(toJson{{text='Warning! ',color='yellow'},{text=filename..'.lua was not the last script to register a '..event..' event! For task.lua to function properly, it must have the last TICK and RENDER events, otherwise you are risking "overran resource limits" errors. Make sure your TICK and RENDER events are registered before the ENTITY_INIT event has run. You can get rid of this warning by going into task.lua and removing the event log section which is marked with comments.\n',color='white'}})
+        end
+    end
+end
+local figuraMetatablesEventsAPI__newindex = figuraMetatables.EventsAPI.__newindex
+local figuraMetatablesEventsAPI__index = figuraMetatables.EventsAPI.__index
+figuraMetatables.EventsAPI.__newindex = function(self,key,...)
+    check_event(key)
+    return figuraMetatablesEventsAPI__newindex(self,key,...)
+end
+figuraMetatables.EventsAPI.__index = function(self,key)
+    check_event(key)
+    return figuraMetatablesEventsAPI__index(self,key)
+end
+-- Event log end
+
+--- Create new render event that only runs once per world render.
+--- Prevents resource overrun error that can usually happen if render event is called multiple
+--- times per frame in different contexts.
+---@param f fun(delta: number, ctx: string, matrix: Matrix4)
+local function SingleRender(f)
+    table.insert(render_events, f)
+end
+
+--- Perform as many processes as allowed by max
+---@param max number
 local function step(max)
     repeat
         local current = stack[#stack]
@@ -28,11 +64,15 @@ function events.entity_init() -- register late
     local lastframe = 0
     function events.tick() step(maxtick) end
     function events.world_render() frame = frame + 1 end
-    function events.render()
+    function events.render(delta, ctx, matrix)
         if lastframe == frame then return end
         lastframe = frame
+        for _, render in ipairs(render_events) do
+            render(delta, ctx, matrix)
+        end
         step(maxrender)
     end
+    events_registered = true
 end
 
 ---Simple progress bar shown in the ActionBar
@@ -98,4 +138,4 @@ local function While(condition,process,callback)
     table.insert(stack,{process=process,callback=callback,i=1,condition=condition})
 end
 
-return setmetatable({ProgressBar=ProgressBar,While=While,For=For},{__call=For})
+return setmetatable({ProgressBar=ProgressBar,While=While,For=For,SingleRender=SingleRender},{__call=For})
